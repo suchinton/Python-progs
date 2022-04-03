f6b20139c67c3ab84392931043b2009b391971dd
suchinton <suchinton.chakravarty@s.amity.edu>
2022-04-03 19:38:19 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-course = 'Python for Beginners'
-#for 
-print(course['0'])
+course = 'python for beginers'
+print(len(course))
+print(course.capitalize())
+print(course.upper())
+print(course.lower())
+print(course.find('b'))
+
+course = course.upper()
+print(course)
